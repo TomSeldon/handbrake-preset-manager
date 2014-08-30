7e6a64bc02cb829e0bb0baa4c5a8d3cd917bc9f9
Tom Seldon <tom@tomseldon.co.uk>
2014-08-30 20:37:09 +0100

    Added directive for accordion items

--- app/components/ui/accordion/accordion-directive.js
+++ app/components/ui/accordion/accordion-directive.js
@@ -17,13 +17,28 @@ goog.provide('hpm.ui.accordion.Directive.factory');
  *
  * @constructor
  */
-hpm.ui.accordion.Directive = function() {};
+hpm.ui.accordion.Directive = function() {
+    this.link = this.link.bind(this);
+};
 
 /**
  * @param {angular.Scope} scope
  * @param {angular.Element} element
  */
 hpm.ui.accordion.Directive.prototype.link = function(scope, element) {
+    // Init accordion
+    this.initAccordion(element);
+
+    // On last ng-repeat of accordion item, init accordion
+    scope.$on('hpm.ui.accordion.item.last', function() {
+        this.initAccordion(element);
+    }.bind(this));
+};
+
+/**
+ * @param {angular.Element} element
+ */
+hpm.ui.accordion.Directive.prototype.initAccordion = function(element) {
     // Initialise Semantic UI accordion
     angular.element(element).accordion();
 };
@@ -39,6 +54,8 @@ hpm.ui.accordion.Directive.factory = function()
     var directive = new hpm.ui.accordion.Directive();
 
     return {
+
         link: directive.link
+
     };
 };
--- app/components/ui/accordion/accordion-module.js
+++ app/components/ui/accordion/accordion-module.js
@@ -10,13 +10,21 @@
 goog.provide('hpm.ui.accordion.module');
 
 goog.require('hpm.ui.accordion.Directive.factory');
+goog.require('hpm.ui.accordion.item.Directive.factory');
 
 /**
  * @type {angular.Module}
  */
 hpm.ui.accordion.module = angular.module('hpm.ui.accordion', []);
 
+// Accordion directive
 hpm.ui.accordion.module.directive(
     'uiAccordion',
     hpm.ui.accordion.Directive.factory
+);
+
+// Accordion item directive
+hpm.ui.accordion.module.directive(
+    'uiAccordionItem',
+    hpm.ui.accordion.item.Directive.factory
 );
