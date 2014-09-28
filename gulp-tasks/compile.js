1c852d64887a350422ebf6d2cca40d876f3d0b59
Tom Seldon <tom@tomseldon.co.uk>
2014-09-28 14:54:33 +0100

    Move Closure cofig out of Gulp task

--- gulp-tasks/compile.js
+++ gulp-tasks/compile.js
@@ -10,52 +10,7 @@ var gulp = require('gulp'),
 gulp.task('compile', function() {
     var closureOptions, src;
 
-    closureOptions = {
-
-        jar: paths.vendor +
-            '/closure-compiler/lib/vendor/compiler.jar',
-
-        compilation_level: 'SIMPLE_OPTIMIZATIONS',
-
-        language_in: 'ECMASCRIPT5_STRICT',
-
-        angular_pass: true,
-
-        externs: [
-            paths.vendor +
-                '/closure-compiler-externs/contrib/externs/angular-1.2.js',
-
-            paths.vendor +
-                '/closure-compiler-externs/contrib/externs/jquery-1.9.js',
-
-            __dirname + '/../custom-externs/semantic-ui.js',
-
-            __dirname + '/../custom-externs/breeze.js'
-        ],
-
-        create_source_map: paths.build.directory +
-            '/' + paths.build.filename + '.map',
-
-        source_map_format: 'V3',
-
-        generate_exports: true,
-
-        manage_closure_dependencies: true,
-
-        js_output_file: paths.build.directory + '/' +
-            paths.build.filename,
-
-        output_wrapper: '(function(){%output%})()' +
-            '//# sourceMappingURL=/assets/js/app.min.js.map',
-
-        define: [
-
-            // State debugging
-            'hpm.debug.state.module.HPM_DEBUG_UI_STATE=false'
-
-        ]
-
-    };
+    closureOptions = require(__dirname + '/../closure.conf');
 
     src = ['bower_components/closure-library/closure/goog/base.js']
         .concat(paths.app)
